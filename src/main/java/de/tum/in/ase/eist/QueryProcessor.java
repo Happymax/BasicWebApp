bab163e756e98e55197c046f60aded624ad1762f
Yunzhi Wang <happymax01@icloud.com>
2022-07-13 17:34:43 +0200

    2 try

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,9 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
+        } else if (query.contains("%20what%20is%2013%20plus%2010")) {
+            return "23";
+        } else {
             return "";
         }
     }
